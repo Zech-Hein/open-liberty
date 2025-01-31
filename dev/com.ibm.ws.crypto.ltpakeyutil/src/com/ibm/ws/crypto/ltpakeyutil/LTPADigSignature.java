4657d6cab69e93cd479558f6e284b2762d296ff4
Zechariah Hein <zechariah.hein@ibm.com>
2025-01-31 15:04:54 -0600

    update LTPA public and private RSA key size to 4096 for FIPS

--- dev/com.ibm.ws.crypto.ltpakeyutil/src/com/ibm/ws/crypto/ltpakeyutil/LTPADigSignature.java
+++ dev/com.ibm.ws.crypto.ltpakeyutil/src/com/ibm/ws/crypto/ltpakeyutil/LTPADigSignature.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1997, 2024 IBM Corporation and others.
+ * Copyright (c) 1997, 2025 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -18,50 +18,50 @@ import com.ibm.ws.common.crypto.CryptoUtils;
 
 final class LTPADigSignature {
 
-	static int keySize = (CryptoUtils.isFips140_3Enabled() ? 256 : 128);
+    static int keySize = (CryptoUtils.isFips140_3Enabled() ? 512 : 128);
 
-	static byte[][] testRawPubKey = null;
-	static byte[][] testRawPrivKey = null;
-	static MessageDigest md1 = CryptoUtils.getMessageDigestForLTPA();
-	static private Object lockObj1 = new Object();
-	static long created = 0;
-	static long cacheHits = 0;
+    static byte[][] testRawPubKey = null;
+    static byte[][] testRawPrivKey = null;
+    static MessageDigest md1 = CryptoUtils.getMessageDigestForLTPA();
+    static private Object lockObj1 = new Object();
+    static long created = 0;
+    static long cacheHits = 0;
 
-	public LTPADigSignature() {
-		super();
-	}
+    public LTPADigSignature() {
+        super();
+    }
 
-	static void generateRSAKeys(byte[][] rsaPubKey, byte[][] rsaPrivKey) {
-		byte[][] rsaKey = LTPACrypto.rsaKey(keySize, true, true); // 64 is 512, 128
-																	// is 1024
+    static void generateRSAKeys(byte[][] rsaPubKey, byte[][] rsaPrivKey) {
+        byte[][] rsaKey = LTPACrypto.rsaKey(keySize, true, true); // 64 is 512, 128
+                                                                  // is 1024
 
-		rsaPrivKey[0] = rsaKey[0];
-		rsaPrivKey[2] = rsaKey[2];
-		rsaPrivKey[4] = rsaKey[3];
-		rsaPrivKey[3] = rsaKey[4];
-		rsaPrivKey[5] = rsaKey[5];
-		rsaPrivKey[6] = rsaKey[6];
-		rsaPrivKey[7] = rsaKey[7];
+        rsaPrivKey[0] = rsaKey[0];
+        rsaPrivKey[2] = rsaKey[2];
+        rsaPrivKey[4] = rsaKey[3];
+        rsaPrivKey[3] = rsaKey[4];
+        rsaPrivKey[5] = rsaKey[5];
+        rsaPrivKey[6] = rsaKey[6];
+        rsaPrivKey[7] = rsaKey[7];
 
-		rsaPubKey[0] = rsaKey[0];
-		rsaPubKey[1] = rsaKey[2];
-	}
+        rsaPubKey[0] = rsaKey[0];
+        rsaPubKey[1] = rsaKey[2];
+    }
 
-	static boolean verify(byte[] mesg, byte[] signature, LTPAPublicKey pubKey) throws Exception {
-		byte[][] rsaPubKey = pubKey.getRawKey();
-		byte[] data;
-		synchronized (lockObj1) {
-			data = md1.digest(mesg);
-		}
-		return LTPACrypto.verifyISO9796(rsaPubKey, data, 0, data.length, signature, 0, signature.length);
-	}
+    static boolean verify(byte[] mesg, byte[] signature, LTPAPublicKey pubKey) throws Exception {
+        byte[][] rsaPubKey = pubKey.getRawKey();
+        byte[] data;
+        synchronized (lockObj1) {
+            data = md1.digest(mesg);
+        }
+        return LTPACrypto.verifyISO9796(rsaPubKey, data, 0, data.length, signature, 0, signature.length);
+    }
 
-	static LTPAKeyPair generateLTPAKeyPair() {
-		byte[][] rsaPubKey = new byte[2][];
-		byte[][] rsaPrivKey = new byte[8][];
-		generateRSAKeys(rsaPubKey, rsaPrivKey);
-		LTPAPublicKey pubKey = new LTPAPublicKey(rsaPubKey);
-		LTPAPrivateKey privKey = new LTPAPrivateKey(rsaPrivKey);
-		return new LTPAKeyPair(pubKey, privKey);
-	}
+    static LTPAKeyPair generateLTPAKeyPair() {
+        byte[][] rsaPubKey = new byte[2][];
+        byte[][] rsaPrivKey = new byte[8][];
+        generateRSAKeys(rsaPubKey, rsaPrivKey);
+        LTPAPublicKey pubKey = new LTPAPublicKey(rsaPubKey);
+        LTPAPrivateKey privKey = new LTPAPrivateKey(rsaPrivKey);
+        return new LTPAKeyPair(pubKey, privKey);
+    }
 }
--- dev/com.ibm.ws.crypto.ltpakeyutil/src/com/ibm/ws/crypto/ltpakeyutil/LTPAPublicKey.java
+++ dev/com.ibm.ws.crypto.ltpakeyutil/src/com/ibm/ws/crypto/ltpakeyutil/LTPAPublicKey.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1997, 2024 IBM Corporation and others.
+ * Copyright (c) 1997, 2025 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -22,72 +22,72 @@ import com.ibm.ws.common.crypto.CryptoUtils;
  */
 public final class LTPAPublicKey implements PublicKey {
 
-	private static final boolean fipsEnabled = CryptoUtils.isFips140_3Enabled();
-	private static final long serialVersionUID = 6585779055758956436L;
-	private static final int MODULUS = 0;
-	private static final int EXPONENT = 1;
-	private static final int MODULUS_LENGTH = (fipsEnabled ? 257 : 129);
-	private static final int EXPONENT_LENGTH = 3;
-	private final byte[][] rawKey;
-	private final byte[] encodedKey;
+    private static final boolean fipsEnabled = CryptoUtils.isFips140_3Enabled();
+    private static final long serialVersionUID = 6585779055758956436L;
+    private static final int MODULUS = 0;
+    private static final int EXPONENT = 1;
+    private static final int MODULUS_LENGTH = (fipsEnabled ? 513 : 129);
+    private static final int EXPONENT_LENGTH = 3;
+    private final byte[][] rawKey;
+    private final byte[] encodedKey;
 
-	LTPAPublicKey(byte[][] rawKey) {
-		this.rawKey = rawKey;
-		this.encodedKey = encode();
-	}
+    LTPAPublicKey(byte[][] rawKey) {
+        this.rawKey = rawKey;
+        this.encodedKey = encode();
+    }
 
-	public LTPAPublicKey(byte[] encodedKey) {
-		this.encodedKey = encodedKey.clone();
-		this.rawKey = decode(encodedKey);
-	}
+    public LTPAPublicKey(byte[] encodedKey) {
+        this.encodedKey = encodedKey.clone();
+        this.rawKey = decode(encodedKey);
+    }
 
-	/**
-	 * encoding/decoding are based on non-standard LTPA specific algorithm.
-	 * concatenates byte arrays of raw key to a format that can be decoded based on
-	 * length of each component.
-	 *
-	 * @param encodedPublicKey The encoded key
-	 */
-	private byte[][] decode(byte[] encodedPublicKey) {
-		byte[][] decodedKey = new byte[2][];
-		decodedKey[MODULUS] = new byte[MODULUS_LENGTH];
-		decodedKey[EXPONENT] = new byte[EXPONENT_LENGTH];
-		System.arraycopy(encodedPublicKey, 0, decodedKey[MODULUS], 0, MODULUS_LENGTH);
-		System.arraycopy(encodedPublicKey, MODULUS_LENGTH, decodedKey[EXPONENT], 0, EXPONENT_LENGTH);
-		return decodedKey;
-	}
+    /**
+     * encoding/decoding are based on non-standard LTPA specific algorithm.
+     * concatenates byte arrays of raw key to a format that can be decoded based on
+     * length of each component.
+     *
+     * @param encodedPublicKey The encoded key
+     */
+    private byte[][] decode(byte[] encodedPublicKey) {
+        byte[][] decodedKey = new byte[2][];
+        decodedKey[MODULUS] = new byte[MODULUS_LENGTH];
+        decodedKey[EXPONENT] = new byte[EXPONENT_LENGTH];
+        System.arraycopy(encodedPublicKey, 0, decodedKey[MODULUS], 0, MODULUS_LENGTH);
+        System.arraycopy(encodedPublicKey, MODULUS_LENGTH, decodedKey[EXPONENT], 0, EXPONENT_LENGTH);
+        return decodedKey;
+    }
 
-	private byte[] encode() {
-		int publicKeyLength = MODULUS_LENGTH + EXPONENT_LENGTH;
-		byte[] encodedPublicKey = new byte[publicKeyLength];
-		System.arraycopy(rawKey[MODULUS], 0, encodedPublicKey, 0, MODULUS_LENGTH);
-		System.arraycopy(rawKey[EXPONENT], 0, encodedPublicKey, MODULUS_LENGTH, EXPONENT_LENGTH);
-		return encodedPublicKey;
-	}
+    private byte[] encode() {
+        int publicKeyLength = MODULUS_LENGTH + EXPONENT_LENGTH;
+        byte[] encodedPublicKey = new byte[publicKeyLength];
+        System.arraycopy(rawKey[MODULUS], 0, encodedPublicKey, 0, MODULUS_LENGTH);
+        System.arraycopy(rawKey[EXPONENT], 0, encodedPublicKey, MODULUS_LENGTH, EXPONENT_LENGTH);
+        return encodedPublicKey;
+    }
 
-	/** {@inheritDoc} */
-	@Override
-	public final String getAlgorithm() {
-		return (fipsEnabled ? "RSA/SHA-256" : "RSA/SHA-1");
-	}
+    /** {@inheritDoc} */
+    @Override
+    public final String getAlgorithm() {
+        return (fipsEnabled ? "RSA/SHA-512" : "RSA/SHA-1");
+    }
 
-	/** {@inheritDoc} */
-	@Override
-	public final byte[] getEncoded() {
-		return encodedKey.clone();
-	}
+    /** {@inheritDoc} */
+    @Override
+    public final byte[] getEncoded() {
+        return encodedKey.clone();
+    }
 
-	/** {@inheritDoc} */
-	@Override
-	public final String getFormat() {
-		return "LTPAFormat";
-	}
+    /** {@inheritDoc} */
+    @Override
+    public final String getFormat() {
+        return "LTPAFormat";
+    }
 
-	protected final byte[][] getRawKey() {
-		if (rawKey == null) {
-			return null;
-		} else {
-			return rawKey.clone();
-		}
-	}
+    protected final byte[][] getRawKey() {
+        if (rawKey == null) {
+            return null;
+        } else {
+            return rawKey.clone();
+        }
+    }
 }
